a03fa2c3364bb49509edeb7a7040eae8cfcbecb0
Saqib Zafar <saqib@adly.com>
2025-10-30 03:05:12 +0500

    Add .gitignore; configure GitHub Pages

--- node_modules/react-slick/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _slider = _interopRequireDefault(require("./slider"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var _default = exports["default"] = _slider["default"];
